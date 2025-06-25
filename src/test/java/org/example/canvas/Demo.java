39bfc760b893a25f6007a85250adf03447a5a1dd
adalid choque <adalid.choque@ucb.edu.bo>
2025-06-25 14:54:48 -0400

    login, create course

--- src/test/java/org/example/canvas/Demo.java
+++ src/test/java/org/example/canvas/Demo.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.canvas;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -65,5 +65,28 @@ public class Demo {
         ChromeDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(15));
         driver.get("https://canvas.instructure.com");
+        driver.findElement(By.cssSelector("input#IDToken2")).sendKeys("adalid.choque.cuba@gmail.com");
+        driver.findElement(By.cssSelector("input#IDToken3")).sendKeys("CambASAdAdCu790");
+        driver.findElement(By.cssSelector("#root span form span:nth-child(2) button")).click();
+
+        // Click en el enlace de courses
+        driver.findElement(By.cssSelector("#global_nav_courses_link")).click();
+        
+        // Click en All Courses
+        driver.findElement(By.cssSelector("a[href='/courses']")).click();
+        
+        // Click en botón Course
+        driver.findElement(By.cssSelector("#start_new_course")).click();
+        
+        // Escribir nombre del curso
+        driver.findElement(By.cssSelector("#TextInput___0")).sendKeys("Demo");
+        
+        // Click en botón Create - probar selector simple
+        driver.findElement(By.cssSelector("div[class*='modalFooter'] button:last-child")).click();
+
+
+        //finalizar el drier de pruebas 
+        driver.quit();
+
     }
 }
